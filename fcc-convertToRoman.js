3908aae1469966c5ae875c8ebec4cd929a2d56ef
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-03-16 01:43:28 +0700

    create new repository

---
+++ fcc-convertToRoman.js
@@ -0,0 +1,28 @@
+var romanRule = {
+    1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX',
+    10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'LXXX', 90: 'XC',
+    100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCC', 800: 'DCCC', 900: 'CM',
+    1000: 'M', 2000: 'MM', 3000: 'MMM', 4000: 'MMMM', 5000: 'MMMMM', 6000: 'MMMMMM'
+};
+
+function convertEachCharacter(number) {
+    return romanRule[number];
+}
+
+console.log(convertEachCharacter("1000"));
+function convertToRoman(num) {
+    var result = '';
+    var arrNumber = num.toString().split('').reverse();
+    console.log(arrNumber);
+    for (var i = arrNumber.length - 1; i >= 0; i--) {
+        arrNumber[i] = arrNumber[i] + '0'.repeat(i);
+        console.log(arrNumber[i]);
+        if (arrNumber[i] > 0) {
+            result = result + convertEachCharacter(arrNumber[i]);
+        }
+    }
+    console.log(result);
+}
+
+convertToRoman(500);
+
